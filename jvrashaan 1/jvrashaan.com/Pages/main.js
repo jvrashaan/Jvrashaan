3afd00f3bdff0058d2793f5faf1a4e9221272bff
jvrashaan <jvrashaan@gmailcom>
2018-12-04 13:11:17 -0500

    merge

---
+++ jvrashaan 1/jvrashaan.com/Pages/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  //Home page
+
+
+
+
+  //About me Page
+  $('#myphoto').hide();
+  $('.table').hide();
+  $('#bu').hide();
+  $('#myphoto').fadeIn(2000);
+  $('.table').fadeIn(2000);
+  $('#bu').fadeIn(2000);
+
+});
